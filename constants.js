d64550561154de4fec55bf0dcc4dafbfc371a720
Fuat Güzel <fuatguzeel@gmail.com>
2020-12-13 16:08:02 +0300

    SearchBar eklendi.

--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
 export const tabBackgroundColor = "#faf7f7";
 export const tabIconColor = "#e63737";
 export const appBackgroundColor = "#fcfafa";
+export const searchBarBackgroundColor = 'red';
--- screens/FilterRecipe.js
+++ screens/FilterRecipe.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-
+import { SafeAreaView, Text, View, StyleSheet } from "react-native";
+import { SearchBar, ListItem } from "react-native-elements";
+import { tabIconColor } from "../constants";
 export class FilterRecipe extends Component {
   render() {
     return (
-      <View>
-        <Text> Filter Recipe </Text>
-      </View>
+      <SafeAreaView>
+        <View style={{ marginTop: 25 }}>
+          <SearchBar
+            style={styles.searchBar}
+            platform="android"
+            //placeholderTextColor={tabIconColor}
+            placeholder='Search..'
+            lightTheme
+            round />
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  searchBar: {
+    color: tabIconColor,
+  }
+})
 export default FilterRecipe;
